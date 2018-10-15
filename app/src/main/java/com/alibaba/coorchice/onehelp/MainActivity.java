3423fd2233f17374ab1986d6167631f249fbbba8
纽特 <coorchice.cb@alibaba-inc.com>
2018-10-15 15:01:05 +0800

    --

---
+++ app/src/main/java/com/alibaba/coorchice/onehelp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.alibaba.coorchice.onehelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
